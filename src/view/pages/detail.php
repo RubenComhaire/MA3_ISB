a8f5582dc64bb552dfe9f0e8c8dc4a7d8a930932
RubenComhaire <ruben.comhaire@student.howest.be>
2019-01-25 23:49:42 +0100

    detail pagina

--- src/view/pages/detail.php
+++ src/view/pages/detail.php
@@ -10,24 +10,75 @@
             <li><a class="menu__links" href="#">faq</a></li>
         </ul>
     </nav>
-    <div class="detail__head">
-        <picture class="events__block4--image">
-            <source media="(max-width: 768px)" srcset="./assets/img/detail/cie_sitting_duck_sm.webp" type="image/webp">
+    <div class="detail">
+        <a class="detail__terug" href="index.php?page=programma"><span class="detail__terug-replace">terug</span></a>
+        <picture class="detail__head__image">
+            <source media="(max-width: 1024px)" srcset="./assets/img/detail/cie_sitting_duck_sm.webp" type="image/webp">
             <source media="(max-width: 1920px)" srcset="./assets/img/detail/cie_sitting_duck_lg.webp" type="image/webp">
-            <source media="(max-width: 768px)" srcset="./assets/img/detail/cie_sitting_duck_sm.png">
+            <source media="(max-width: 1024px)" srcset="./assets/img/detail/cie_sitting_duck_sm.png">
             <source media="(max-width: 1920px)" srcset="./assets/img/detail/cie_sitting_duck_lg.png">
-
-            <img class="block4-image" src="./assets/img/home/zondag_big.jpg"
+            <img class="detail__image" src="./assets/img/detail/cie_sitting_duck_lg.png"
                 alt="een man en vrouw samen op 1 fiets. De man houdt de vrouw in zijn armen.">
         </picture>
-
+        <p class="detail__tag"><span class="light">&mdash; </span>voorstelling</p>
+        <p class="detail__performer">Cie Sitting Duck <span class="detail__nat">(B)</span></p>
+        <p class="detail__act">Chringuito Paradise</p>
+        <p class="detail__datum">Vrijdag 24 Augustus</p>
+        <div class="detail__uuren">
+            <p class="detail__info-titel">Speeluren</p>
+            <p class="detail__info-text">18:45h - 19:30h</p>
+            <p class="detail__info-text">21:15h - 22:00h</p>
+        </div>
+        <div class="detail__locatie">
+            <p class="detail__info-titel">Locatie</p>
+            <p class="detail__info-text">Locatie 1:KA/BS De Bever</p>
+        </div>
     </div>
-    <aside class="programma__social">
-        <a class="social__link1__p" target="_blank" href="https://www.facebook.com/straattheaterfestivalbeveren/"></a>
-        <br>
-        <a class="social__link2__p" target="_blank" href="https://twitter.com/hashtag/beversefeesten?src=hash"></a>
-        <br>
-        <a class="social__link3__p" target="_blank"
-            href="https://www.instagram.com/explore/locations/375292120/beverse-feesten/"></a>
-    </aside>
 </header>
+<main>
+    <section class="act">
+        <p class="act__duur">45 minuten</p>
+        <p class="act__titel">Chringuito Paradise</p>
+        <div class="act__info">
+            <hr class="act__info__line" />
+            <p class="act__info__text">
+                Alles staat klaar voor een geslaagde openingsplechtigheid
+                van de cocktailbar. De champagne staat koud.
+                Maar een ongeluk schuilt altijd in een klein hoekje…
+            </p>
+        </div>
+    </section>
+    <section class="performer">
+        <div class="performer__vidbox">
+            <embed class="performer__vidbox__vid"
+                src="http://www.youtube.com/v/Nac4Y2dNq80?version=3&amp;hl=en_US&amp;rel=0&amp;autohide=1&amp;autoplay=1"
+                wmode="transparent" type="application/x-shockwave-flash" width="1280" height="720"
+                allowfullscreen="true" title="Adobe Flash Player">
+        </div>
+        <article class="performer__info">
+            <p class="performer__info__titel">meer weten over</p>
+            <p class="performer__info__name">Cie sitting duck</p>
+            <a class="performer__info__link" href="#">www.chiringuitoparadise.com</a>
+            <p class="performer__info__box-1">Sitting Duck is een Belgische
+                compagnie opgericht in 2016
+                door Gonzalo Alarcón van Cie.
+                Hopla Circus en Joris Verbeeren
+                van Cie. Shake That.
+                Genspireerd door de grote
+                meesters van de komische cinema
+                zoals B.Keaton, Laurel & Hardy.
+            </p>
+            <p class="performer__info__box-2">Zeker ook Peter Sellers en
+                Jackie Chan onder de meer
+                hedendaagse, doken ze in
+                de wereld van de fysieke humor.
+                Jongleren, slapstick, accidenten,
+                objectmanipulatie en gags worden
+                onderzocht als bron van spel en humor.
+                De compagnie's specialiteiten
+                en voorliefdes zijn jongleren
+                met flessen, clownerie, slapstick
+                en impossible tricks!</p>
+        </article>
+    </section>
+</main>
